d8d1ca33613b4d45cc7a4548f697d09ff899c734
Masahisa Kobayashi <watertrans@gmail.com>
2021-03-27 11:38:31 +0900

    Update database user and password

--- schema/setup.sql
+++ schema/setup.sql
@@ -5,38 +5,38 @@ GO
 CREATE DATABASE [DailyReport] COLLATE Japanese_XJIS_100_CI_AS
 GO
 
--- 開発用ログイン作成
-CREATE LOGIN [dev] WITH PASSWORD='dev'
-GO
-
--- ユニットテスト用ログイン作成
-CREATE LOGIN [unittest] WITH PASSWORD='unittest'
-GO
-
 USE [DailyReport]
 GO
 
+-- 開発用ログイン作成
+CREATE LOGIN [daily_report_dev] WITH PASSWORD='ZXcvbNM#1'
+GO
+
 -- 開発用ユーザー作成
-CREATE USER [dev] FOR LOGIN [dev] WITH DEFAULT_SCHEMA=[dev]
+CREATE USER [daily_report_dev] FOR LOGIN [daily_report_dev] WITH DEFAULT_SCHEMA=[dev]
 GO
 
 -- 開発用スキーマ作成
-CREATE SCHEMA [dev] AUTHORIZATION [dev]
+CREATE SCHEMA [dev] AUTHORIZATION [daily_report_dev]
 GO
 
 -- 開発用ユーザーへの権限付与
-GRANT CREATE TABLE TO [dev]
+GRANT CREATE TABLE TO [daily_report_dev]
+GO
+
+-- ユニットテスト用ログイン作成
+CREATE LOGIN [daily_report_ut] WITH PASSWORD='ZXcvbNM#2'
 GO
 
 -- ユニットテスト用ユーザー作成
-CREATE USER [unittest] FOR LOGIN [unittest] WITH DEFAULT_SCHEMA=[unittest]
+CREATE USER [daily_report_ut] FOR LOGIN [daily_report_ut] WITH DEFAULT_SCHEMA=[ut]
 GO
 
 -- ユニットテスト用スキーマ作成
-CREATE SCHEMA [unittest] AUTHORIZATION [unittest]
+CREATE SCHEMA [ut] AUTHORIZATION [daily_report_ut]
 GO
 
 -- ユニットテスト用ユーザーへの権限付与
-GRANT CREATE TABLE TO [unittest]
+GRANT CREATE TABLE TO [daily_report_ut]
 GO
 
