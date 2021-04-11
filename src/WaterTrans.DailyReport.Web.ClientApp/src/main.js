8d832c9e5432fb1af5ad866a69a123c7cfc827d8
Masahisa Kobayashi <watertrans@gmail.com>
2021-04-11 16:08:48 +0900

    Add Sass and PrimeVue

--- src/WaterTrans.DailyReport.Web.ClientApp/src/main.js
+++ src/WaterTrans.DailyReport.Web.ClientApp/src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import './assets/layout/layout.scss';
 
-createApp(App).use(router).mount('#app')
+import { createApp } from 'vue'
+import router from './router'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Ripple from 'primevue/ripple';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true });
+app.use(ToastService);
+app.use(router);
+
+app.directive('ripple', Ripple);
+
+app.component('Toast', Toast);
+
+app.mount('#app');
