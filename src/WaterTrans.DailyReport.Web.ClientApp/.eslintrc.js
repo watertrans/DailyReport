40b36ae14764b4ebc0c634438d10589c33e1a6ed
Masahisa Kobayashi <watertrans@gmail.com>
2021-04-11 23:42:11 +0900

    Add eslint support

--- src/WaterTrans.DailyReport.Web.ClientApp/.eslintrc.js
+++ src/WaterTrans.DailyReport.Web.ClientApp/.eslintrc.js
@@ -11,6 +11,13 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'semi': ['error', 'always'],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'quotes': ['warn', 'single'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
@@ -25,4 +32,4 @@ module.exports = {
       }
     }
   ]
-}
+};
--- src/WaterTrans.DailyReport.Web.ClientApp/src/main.js
+++ src/WaterTrans.DailyReport.Web.ClientApp/src/main.js
@@ -4,22 +4,30 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import './assets/layout/layout.scss';
 
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import ConfirmDialog from 'primevue/confirmdialog';
+import ConfirmPopup from 'primevue/confirmpopup';
+import ConfirmationService from 'primevue/confirmationservice';
 import Ripple from 'primevue/ripple';
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
 
 const app = createApp(App);
 
 app.use(PrimeVue, { ripple: true });
+app.use(ConfirmationService);
 app.use(ToastService);
 app.use(router);
 
+app.directive('tooltip', Tooltip);
 app.directive('ripple', Ripple);
 
+app.component('ConfirmDialog', ConfirmDialog);
+app.component('ConfirmPopup', ConfirmPopup);
 app.component('Toast', Toast);
 
 app.mount('#app');
