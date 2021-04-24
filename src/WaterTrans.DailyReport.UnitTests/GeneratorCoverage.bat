14a0154e38497a620889e00987896579cc7172c5
Masahisa Kobayashi <watertrans@gmail.com>
2021-04-24 13:05:49 +0900

    Add Coverage Report

--- src/WaterTrans.DailyReport.UnitTests/GeneratorCoverage.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-dotnet tool install -g dotnet-reportgenerator-globaltool
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./bin/
-reportgenerator -reports:./bin/coverage.cobertura.xml -targetdir:./bin/coveragereport -reporttypes:Html
